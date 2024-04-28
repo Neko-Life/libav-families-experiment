1bb5378a6f5c37426a4f7cc82c871b5d2e036a6e
Neko-Life <nekolife123579@gmail.com>
2024-04-28 14:33:54 +0700

    initial commit

---
+++ src/main.cpp
@@ -0,0 +1,17 @@
+#include <cstdint>
+#include <cstdio>
+#include "opus/opus.h"
+
+int main() {
+  printf("Hello World!");
+
+  int32_t samp_rate = 48000;
+  int channels = 2;
+
+  // init decoder
+  /* int error; */
+  /* OpusDecoder *dec; */
+  /* dec = opus_decoder_create(samp_rate, channels, &error); */
+
+  return 0;
+}
